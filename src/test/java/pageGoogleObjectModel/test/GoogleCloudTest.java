3f5de23489e076c08881fc40873ea72cb4c85c8c
Alena Kemiazhuk <elenazayats1993@gmail.com>
2020-12-22 15:53:30 +0300

    added automation framework with screen

--- src/test/java/pageGoogleObjectModel/test/GoogleCloudTest.java
+++ src/test/java/pageGoogleObjectModel/test/GoogleCloudTest.java
@@ -3,19 +3,17 @@ package pageGoogleObjectModel.test;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pageGoogleObjectModel.driver.DriverSingleton;
-import pageGoogleObjectModel.service.CalculatorDataCreator;
 import pageGoogleObjectModel.service.GoogleCalculatorService;
 import pageGoogleObjectModel.service.GoogleCloudHomeService;
 import pageGoogleObjectModel.service.GoogleMailService;
 import pageGoogleObjectModel.util.RegexUtils;
 import pageGoogleObjectModel.util.TestListener;
-
-import static org.testng.AssertJUnit.assertEquals;
 
 @Listeners({TestListener.class})
 @Getter
@@ -26,7 +24,7 @@ public class GoogleCloudTest {
     WebDriver driver;
     RegexUtils regexUtils;
     String inputTextForSearch = "Google Cloud Platform Pricing Calculator";
-//    String realCost = "Total Estimated Cost: USD 1,082.77 per 1 month";
+    //    String realCost = "Total Estimated Cost: USD 1,082.77 per 1 month";
     String regex = "\\s\\d.+?\\d$|\\s\\d.+?\\d\\s";
 
     @BeforeClass
@@ -37,27 +35,30 @@ public class GoogleCloudTest {
     @SneakyThrows
     @Test
     public void sendEmailTest() {
-        googleCloudHomePageService = new GoogleCloudHomeService(driver);
-        googleCloudHomePageService.getGoogleCloudHomePage().openPage();
-        googleCalculatorPageService = new GoogleCalculatorService(driver);
-        googleMailService = new GoogleMailService(driver);
-        regexUtils = new RegexUtils(driver);
-        googleCloudHomePageService.goToCalculatorPage(inputTextForSearch);
-        googleCalculatorPageService.getCalculatorPage().
-                timeOut(30, googleCalculatorPageService.getXpathNumberOfInstances());
-        googleCalculatorPageService.googleCalculatorFilling(CalculatorDataCreator.creatorCalculatorData());
-        googleCalculatorPageService.calculateEstimate();
-        googleCalculatorPageService.emailEstimate();
-        googleMailService.copyMailAddress();
-        googleCalculatorPageService.sendEstimateOnEmail();
-        String totalCostFromEmail = regexUtils.getCostForCompare(googleMailService.getTotalCostFromSentEmail(), regex);
-        String totalCostFromEstimate = regexUtils.getCostForCompare(googleCalculatorPageService.getTotalCostFromEstimate(), regex);
-        assertEquals(totalCostFromEstimate, totalCostFromEmail);
+//        googleCloudHomePageService = new GoogleCloudHomeService(driver);
+//        googleCloudHomePageService.getGoogleCloudHomePage().openPage();
+//        googleCalculatorPageService = new GoogleCalculatorService(driver);
+//        googleMailService = new GoogleMailService(driver);
+//        regexUtils = new RegexUtils(driver);
+//        googleCloudHomePageService.goToCalculatorPage(inputTextForSearch);
+//        googleCalculatorPageService.getCalculatorPage().
+//                timeOut(30, googleCalculatorPageService.getXpathNumberOfInstances());
+//        googleCalculatorPageService.googleCalculatorFilling(CalculatorDataCreator.creatorCalculatorData())
+//                .calculateEstimate();
+//        googleCalculatorPageService.emailEstimate();
+//        googleMailService.copyMailAddress();
+//        googleCalculatorPageService.sendEstimateOnEmail();
+//        String totalCostFromEmail = regexUtils
+//                .getCostForCompare(googleMailService.getTotalCostFromSentEmail(), regex);
+//        String totalCostFromEstimate = regexUtils
+//                .getCostForCompare(googleCalculatorPageService.getTotalCostFromEstimate(), regex);
+//        assertEquals(totalCostFromEstimate, totalCostFromEmail);
+        Assert.assertTrue(false);
     }
 
     @AfterClass
-    public void closeBrowser(){
-       DriverSingleton.closeDriver();
+    public void closeBrowser() {
+        DriverSingleton.closeDriver();
     }
 
 
